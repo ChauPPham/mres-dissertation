8355474224219312fe6ae314bfd60c9c466277a9
Chau Pham <chau.ppham7@gmail.com>
2023-06-15 22:02:36 +0100

    Update exploratory.R

--- exploratory.R
+++ exploratory.R
@@ -1,6 +1,8 @@
-# Date: 06/15/2023
+# Date created: 06/15/2023
 # Author: Chau Pham
 # Purpose: Exploratory coding for MRES dissertation
+
+# Last changes: 06/15/2023
 #--------------------------------------------------------------#
 
 # Load data
