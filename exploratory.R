e568128f0728c7c1552b9b1f29e5faa4992647d4
ChauPPham <96724644+ChauPPham@users.noreply.github.com>
2023-06-18 17:51:42 +0100

    Update exploratory.R
    Worked out how to calculate prob for second iteration.

--- exploratory.R
+++ exploratory.R
@@ -10,6 +10,7 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 require("foreign")
 require("tidyverse")
+require("base")
 
 background <- read.dta("LISS data/avars_201001_EN_2.0p/avars_201001_EN_2.0p.dta")  # background data
 ambiguity <- read.dta("LISS data/bm10a_EN_1.0p/bm10a_EN_1.0p.dta") # ambiguity data
@@ -19,38 +20,42 @@ background_short <- select(background, -c(lftdcat, woonvorm, brutocat, nettocat,
 pool <- right_join(background_short, ambiguity, by = c("nomem_encr" = "nomem_encr", "nohouse_encr" = "nohouse_encr"))
 
 
-##===== Calculate ambiguity indexes
+##===== Calculate ambiguity indices
 # Updating functions
 prob_update <- function(choice, ceil, floor, prob){
     # This function updates the probability of risky gain after each iterations
     if(choice == "Box B"){
         floor = prob
         prob = 1/2*(ceil + prob)
-        assign(floor, floor, envir = parent.env(env))
-        assign(prob, prob, envir = parent.env(env))
+        assign("floor", floor, envir = parent.frame())
+        assign("prob", prob, envir = parent.frame())
         return(prob)
     }
     else {
         ceil = prob
         prob = 1/2*(floor + prob)
-        assign(ceil, ceil, envir = parent.env(env))
-        assign(prob, prob, envir = parent.env(env))
+        assign("ceil", ceil, envir = parent.frame())
+        assign("prob", prob, envir = parent.frame())
         return(prob)
     }
 }
+
+prob_update_v <- Vectorize(prob_update, vectorize.args = "choice")
 
 get_m <- function(range, number_of_colours, dataset){
     col_range <- sprintf("bm10a%0.3d", range)
     dataset <- dataset %>% rowwise()
     prob <- 1/number_of_colours
     iter <- sym(col_range[length(col_range)])
-    ceil <- 100
+    ceil <- 1
     floor <- 0
     
-    dataset <- dataset %>% mutate("m_{prob}" := ifelse(!!iter == 1, prob, 0))
     dataset <- dataset %>% mutate("m_{prob}" := case_when(
-        iter == 2 ~ prob_update()
+        !!iter == 1 ~ prob,
+        !!iter == 2 ~ prob_update_v(!!sym(col_range[2]), ceil, floor, prob), 
+        !!iter == 3 ~ 
     ))
+    unique(dataset[,dim(dataset)[2]])
 }
 
 
