cf8abec49e0fd4b20cef78b8fcd4dd22214d0efe
ChauPPham <96724644+ChauPPham@users.noreply.github.com>
2023-06-21 20:09:59 +0100

    Add data for both ALP and LISS
    LISS can now run test with risk aversion and ambiguity aversion
    ALP can do the same plus if used a different sub-dataset (which only allows ambiguity-averse and loss-averse) then can elicit how ambiguity-averse and loss-averse affects educational choice

--- exploratory_ALP.R
+++ exploratory_ALP.R
@@ -2,20 +2,42 @@
 # Author: Chau Pham
 # Purpose: Exploratory coding with ALP data for MRES dissertation
 
-# Last changes: 06/20/2023
+# Last changes: 06/21/2023
 #--------------------------------------------------------------#
 
 ##===== Load & clean data
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 require("haven")
+require("readxl")
 require("tidyverse")
 require("base")
 
 background <- read_dta("ALP data/ms90002_public.dta")  # background data
 wave1 <- read_dta("ALP data/ms315_weighted.dta") # wave 1 of the survey on decision quality
 wave2 <- read_dta("ALP data/ms352_weighted.dta") # wave 2 of the survey on decision quality
-merge <- right_join(wave1, wave2, by = "prim_key")
+merge <- inner_join(wave1, wave2, by = "prim_key")
 
-education <- read_dta("ALP data/ms504_weighted.dta")
-pool <- right_join(merge, education, by = "prim_key") %>% filter(!is.na(borninus.x))
+amb <- read_dta("ALP data/ms243_weighted.dta")  # wave 1 (main) of the survey on ambiguity & risk
+amb1 <- read_dta("ALP data/ms338_weighted.dta")  # wave 2 of the survey on ambiguity & risk (test trust)
+amb2 <- read_dta("ALP data/ms339_weighted.dta")  # wave 2 of the survey on ambiguity & risk (test trust)
+
+education <- read_xlsx("ALP data/ms301_weighted.xlsx")
+ed_cols <- grep("seceloopchns_[0-9]+_e0uc", colnames(education))
+
+for (i in ed_cols){
+  for (j in 1:nrow(education)){
+    if (!is.na(pool2[j,i])) {
+      if (pool2[j, i] == 97) pool2[j,i] = -99
+    }
+  }
+}
+
+education$max_ed <- apply(pool2[, ed_cols], 1, max, na.rm = TRUE)
+
+pool1 <- inner_join(amb, education, by = "prim_key")
+pool2 <- inner_join(merge, education, by = "prim_key")
+
+usable1 <- pool1 %>% filter(max_ed >= 0)
+usable2 <- pool2 %>% filter(max_ed >= 0)
+
--- exploratory_LISS.R
+++ exploratory_LISS.R
@@ -2,22 +2,24 @@
 # Author: Chau Pham
 # Purpose: Exploratory coding with LISS data for MRES dissertation
 
-# Last changes: 06/18/2023
+# Last changes: 06/21/2023
 #--------------------------------------------------------------#
 
 ##===== Load & clean data
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-require("foreign")
+require("haven")
 require("tidyverse")
 require("base")
 
-background <- read.dta("LISS data/avars_201001_EN_2.0p.dta")  # background data
-ambiguity <- read.dta("LISS data/bm10a_EN_1.0p.dta") # ambiguity data
-
+background <- read_dta("LISS data/avars_201001_EN_2.0p.dta")  # background data
+family <- read_dta("LISS data/cf10c_EN_1.0p.dta")             # family data
+ambiguity <- read_dta("LISS data/bm10a_EN_1.0p.dta")          # ambiguity data
+timeuse <- read_dta("LISS data/tp19a_EN_1.0p.dta")
 
 background_short <- select(background, -c(lftdcat, woonvorm, brutocat, nettocat, doetmee))
 pool <- right_join(background_short, ambiguity, by = c("nomem_encr" = "nomem_encr", "nohouse_encr" = "nohouse_encr"))
+
 
 
 ##===== Calculate ambiguity indices
@@ -33,13 +35,30 @@ pool <- pool %>% mutate(m_0.1 = bm10a065/100, AA_0.1 = 0.1 - m_0.1)
 # m(0.9), AA(0.9) = 0.9 - m(0.9)
 pool <- pool %>% mutate(m_0.9 = bm10a087/100, AA_0.9 = 0.9 - m_0.9)
 
+ 
+useful_variables <- c(1:10, 17:28, 31, 175:180, 182:183, 226) # If merge before calculating 
+# the ambiguity indices, need to alter this slightly, potentially from 175
+
+# To get the usable data, I choose households that have at least one children living 
+# in household in time-use survey (2019), and filter out households that already have
+# children leaving the household. To account for educational expenditures, I also filter out
+# NAs values of variable tp19a045 which records monthly schooling fees that households pay
+
+usable <- inner_join(pool, timeuse) %>% mutate(child_dif = tp19a003 - aantalki
+) %>% filter(child_dif >= 0, (aantalki+tp19a003 != 0), !is.na(tp19a045)
+) %>% select(all_of(useful_variables), "child_dif") 
+usable$oplcat <- factor(usable$oplcat, levels = c("1", "2", "3", "4", "5", "6"),
+                        labels = c("primary", "vmbo", "havo/vwo", "mbo", "hbo", "wo"))
+
+test <- lm(tp19a045 ~ AA_0.1 + AA_0.5 + AA_0.9 + bm10a004 + oplcat + nettohh_f, data = usable)
+summary(test)
+
 
 ##===== Calculate risk-aversion indices
 
 
 
 
-pool %>% group_by(nohouse_encr) %>% summarise(no_hh_members = n()) %>% arrange(desc(no_hh_members)) %>% filter(no_hh_members > 1)
 
 
 
